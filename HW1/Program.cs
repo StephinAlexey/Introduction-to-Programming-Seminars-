e61d4b7c481f56f0b20c0b78951c4078e385cd21
StephinAlexey <alexeystephin@gmail.com>
2024-06-22 08:54:20 +0300

    gitignore addition

---
+++ HW1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HW1;
+
+class Program
+{
+    enum Colors : byte
+    {
+        Red = 0,
+        Green = 1,
+        Black = 2,
+        White = 4
+    }
+    static void Main(string[] args)
+    {
+        Colors BlackWhite = Colors.Black | Colors.White;
+        Console.WriteLine(BlackWhite);
+    }
+}
---
+++ lesson1/Program.cs
@@ -0,0 +1,30 @@
+﻿// System.Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a == b * b)
+// { Console.WriteLine($"b = {b}, a = {a} => Yes"); }
+// else
+// {
+//     if (b == a * a)
+//     {
+//         Console.WriteLine($"b = {b}, a = {a} => Yes");
+//     }
+//     else
+//         Console.WriteLine($"b = {b}, a = {a} => No");
+
+// }
+
+
+int InsertNumFromUser(string DescriptForNumberInsert)
+{
+    System.Console.WriteLine(DescriptForNumberInsert);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Number = InsertNumFromUser("Введите пограничное число");
+if (Number < 0) Number = -Number;
+for (int i = Number; i > -Number; i--)
+    System.Console.Write(i+", ");
+System.Console.WriteLine(-Number);
